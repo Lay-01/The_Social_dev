e0e84a540cb22f4cc050fd81449e753ab54fb560
Lay-01 <116367960+Lay-01@users.noreply.github.com>
2026-08-12 04:58:50 +0530

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/The_Social_dev/",
   server: {
     port: 3000,
     open: true,
-    base: "/The_Social_dev/",
+    
   
   }
 })
